4f8c3899f182c8c43361650c964e95ae26ffe35c
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-11-20 17:50:52 +0700

    Initial Commit

---
+++ Dolanan.id myGameCatalogue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dolanan.id myGameCatalogue
+//
+//  Created by Dimas Putro on 20/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dolanan.id myGameCatalogue/Dolanan_id_myGameCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dolanan_id_myGameCatalogueApp.swift
+//  Dolanan.id myGameCatalogue
+//
+//  Created by Dimas Putro on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Dolanan_id_myGameCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
