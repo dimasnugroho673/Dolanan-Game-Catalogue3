721c05c8f35c23398683bbe2e7aa2bc071278b05
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-12-10 23:42:00 +0700

    cleaning onboarding view

--- Dolanan.id myGameCatalogue/Modules/Onboarding/View/OnboardingView.swift
+++ Dolanan.id myGameCatalogue/Modules/Onboarding/View/OnboardingView.swift
@@ -13,10 +13,6 @@ import Common
 struct OnboardingView: View {
 
   @Environment(\.presentationMode) private var presentation
-  
-//  @ObservedObject var onboardingPresenter: OnboardingPresenter
-//  @ObservedObject var onboardingPresenter: GetDetailPresenter<UserDomainModel, UserDomainModel, Interactor<UserDomainModel, UserDomainModel, UpdateUserRepository<GetUserLocaleDataSource, UserTransformer>>>
-//  @ObservedObject var onboardingPresenter: GetDetailPresenter
 
   @ObservedObject var onboardingPresenter: UserEditPresenter<Interactor<UserDomainModel, UserDomainModel, UpdateUserRepository<GetUserLocaleDataSource, UserTransformer>>>
 
@@ -28,55 +24,70 @@ struct OnboardingView: View {
   
   var body: some View {
     ZStack {
-      Image(images[activeImage])
-        .resizable()
-        .aspectRatio(contentMode: .fill)
-        .animation(.spring())
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-        .overlay(
-          Rectangle()
-            .fill(
-              LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .top, endPoint: .bottom))
-            .opacity(0.7)
-        )
-        .onReceive(timerShowcase) { _ in
-          self.activeImage = (self.activeImage + 1) % self.images.count
-        }
+      self.imageSlideShowContent
       
-      HStack {
-        VStack {
-          Text(LocalizedLang.onboardingTitle)
-            .foregroundColor(.white)
-            .font(.title)
-            .bold()
-            .multilineTextAlignment(.center)
-          
-          Text("\(LocalizedLang.explore) - \(LocalizedLang.discover) - \(LocalizedLang.play)")
-            .foregroundColor(Color.init(.systemGray3))
-            .font(.subheadline)
-            .padding(.top, 5)
-          
-          Button(action: {
-            UserDefaults.standard.setValue(true, forKey: "UserExist")
-            
-            /// insert data dummy
-            onboardingPresenter.updateUser(request: UserDomainModel(id: "0", name: "Dimas Nugroho Putro", email: "dimasnugroho673@gmail.com", phoneNumber: "082285592029", website: "dimasnugroho673.github.io", githubUrl: "https://github.com/dimasnugroho673", profilePicture: Data()))
-            
-            self.isUserExist = true
-            self.presentation.wrappedValue.dismiss()
-          }, label: {
-            Text(LocalizedLang.getStarted)
-              .font(.body)
-              .fontWeight(.bold)
-          })
-            .buttonStyle(StartOnboardingButtonStyle())
-            .padding(.top, 20)
-        }
-        .padding()
-      }
-      .padding(.bottom, 20)
-      .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height, alignment: .bottom)
+      self.promotionContent
     }
     .ignoresSafeArea(.all)
+  }
+}
+
+extension OnboardingView {
+
+  private var imageSlideShowContent: some View {
+    Image(images[activeImage])
+      .resizable()
+      .aspectRatio(contentMode: .fill)
+      .animation(.spring())
+      .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+      .overlay(
+        Rectangle()
+          .fill(
+            LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .top, endPoint: .bottom))
+          .opacity(0.7)
+      )
+      .onReceive(timerShowcase) { _ in
+        self.activeImage = (self.activeImage + 1) % self.images.count
+      }
+  }
+
+  private var promotionContent: some View {
+    HStack {
+      VStack {
+        Text(LocalizedLang.onboardingTitle)
+          .foregroundColor(.white)
+          .font(.title)
+          .bold()
+          .multilineTextAlignment(.center)
+
+        Text("\(LocalizedLang.explore) - \(LocalizedLang.discover) - \(LocalizedLang.play)")
+          .foregroundColor(Color.init(.systemGray3))
+          .font(.subheadline)
+          .padding(.top, 5)
+
+        Button(action: {
+          UserDefaults.standard.setValue(true, forKey: "UserExist")
+
+          /// insert data dummy
+          self.updateUserData()
+
+          self.isUserExist = true
+          self.presentation.wrappedValue.dismiss()
+        }, label: {
+          Text(LocalizedLang.getStarted)
+            .font(.body)
+            .fontWeight(.bold)
+        })
+          .buttonStyle(StartOnboardingButtonStyle())
+          .padding(.top, 20)
+      }
+      .padding()
+    }
+    .padding(.bottom, 20)
+    .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height, alignment: .bottom)
+  }
+
+  private func updateUserData() {
+    self.onboardingPresenter.updateUser(request: UserDomainModel(id: "0", name: "Dimas Nugroho Putro", email: "dimasnugroho673@gmail.com", phoneNumber: "082285592029", website: "dimasnugroho673.github.io", githubUrl: "https://github.com/dimasnugroho673", profilePicture: Data()))
   }
 }
