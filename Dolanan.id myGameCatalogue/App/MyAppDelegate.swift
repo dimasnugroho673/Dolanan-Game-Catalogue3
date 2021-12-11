198e6c5bf4aace9f67623392595cb490eadb00a3
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-12-11 11:40:25 +0700

    delete print on myappdelegate

--- Dolanan.id myGameCatalogue/App/MyAppDelegate.swift
+++ Dolanan.id myGameCatalogue/App/MyAppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 class MyAppDelegate: NSObject, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    print("hi from app delegate")
-
     return true
   }
 
